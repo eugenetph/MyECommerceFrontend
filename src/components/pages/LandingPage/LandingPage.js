5acc234d153730be41bbc7c2761e498a20375ba1
Eugene Tan <eugenetph@Eugenes-MacBook-Pro.local>
2019-07-09 17:53:04 +0800

    refactor

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,54 +1,14 @@
 import React /*{ useState }*/ from 'react';
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
-import ProtectedRoute from '../../utils/ProtectedRoute'
-
-import ProfilePage from '../ProfilePage/ProfilePage'
-
-import './LandingPage.css';
-
-import {
-  Layout,
-  Input,
-  Avatar
-} from 'antd'
-import 'antd/dist/antd.css';
-
-const { Header, Content, Footer } = Layout
 
 const LandingPage = () => {
 
   return (
-    <div className="App">
-      <Router>
-        <Layout>
-          <Header className='main-header'>
-            <div className='header' style={{ display: 'flex' }}>
-              <div style={{ margin: '0 10px' }}>
-                <img className='logo' src={require('../../../assets/shophero.png')} alt='logo' />
-              </div>
-              <div style={{ margin: '0 10px' }}>
-                <Input className='search' placeholder="Search in MyECommerce" disabled={true} />
-              </div>
-              <div>
-                <Link to='/profile-page'>
-                  <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
-                </Link>
-              </div>
-            </div>
-          </Header>
-          <Content className='main-content'>
-            <h1>{process.env.REACT_APP_API_HOST}</h1>
-            <Switch>
-              <ProtectedRoute isAllowed={true} exact path="/profile-page" render={(props) => <ProfilePage {...props} />} />
-            </Switch>
-          </Content>
-          <Footer className='main-footer'>
-            <h1>Footer</h1>
-          </Footer>
-        </Layout>
-      </Router>
+    <div>
+      <h1>
+        {process.env.REACT_APP_API_HOST}
+      </h1>
     </div>
-  );
+  )
 }
 
 export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/pages/LandingPage/LandingPage';
+import { /*Route,*/ Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import ProtectedRoute from './components/utils/ProtectedRoute'
+
+import './index.css';
+
+import LandingPage from './components/pages/LandingPage/LandingPage';
+import ProfilePage from './components/pages/ProfilePage/ProfilePage'
+
+import {
+  Layout,
+  Input,
+  Avatar
+} from 'antd'
+import 'antd/dist/antd.css';
+
+const { Header, Content, Footer } = Layout
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Layout>
+          <Header className='main-header'>
+            <div className='header' style={{ display: 'flex' }}>
+              <div style={{ margin: '0 10px' }}>
+                <Link to='/landing-page'>
+                  <img className='logo' src={require('./assets/shophero.png')} alt='logo' />
+                </Link>
+              </div>
+              <div style={{ margin: '0 10px' }}>
+                <Input className='search' placeholder="Search in MyECommerce" disabled={true} />
+              </div>
+              <div>
+                <Link to='/profile-page'>
+                  <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
+                </Link>
+              </div>
+            </div>
+          </Header>
+          <Content className='main-content'>
+            <Switch>
+              <ProtectedRoute isAllowed={true} exact path="/landing-page" render={(props) => <LandingPage {...props} />} />
+              <ProtectedRoute isAllowed={true} exact path="/profile-page" render={(props) => <ProfilePage {...props} />} />
+            </Switch>
+          </Content>
+          <Footer className='main-footer'>
+            <h1>Footer</h1>
+          </Footer>
+        </Layout>
+      </Router>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
