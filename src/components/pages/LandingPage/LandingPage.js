58b74cd40bdccaff62b91d31b2904feb2a221889
Eugene Tan <eugenetph@Eugenes-MacBook-Pro.local>
2019-07-09 17:38:33 +0800

    add router

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,9 +1,15 @@
 import React /*{ useState }*/ from 'react';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import ProtectedRoute from '../../utils/ProtectedRoute'
+
+import ProfilePage from '../ProfilePage/ProfilePage'
+
 import './LandingPage.css';
 
 import {
   Layout,
-  Input
+  Input,
+  Avatar
 } from 'antd'
 import 'antd/dist/antd.css';
 
@@ -13,24 +19,34 @@ const LandingPage = () => {
 
   return (
     <div className="App">
-      <Layout>
-        <Header className='main-header'>
-          <div className='header'>
-            <div style={{ margin: '0 10px'}}>
-              <img className='logo' src={require('../../../assets/shophero.png')} alt='logo' />
+      <Router>
+        <Layout>
+          <Header className='main-header'>
+            <div className='header' style={{ display: 'flex' }}>
+              <div style={{ margin: '0 10px' }}>
+                <img className='logo' src={require('../../../assets/shophero.png')} alt='logo' />
+              </div>
+              <div style={{ margin: '0 10px' }}>
+                <Input className='search' placeholder="Search in MyECommerce" disabled={true} />
+              </div>
+              <div>
+                <Link to='/profile-page'>
+                  <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
+                </Link>
+              </div>
             </div>
-            <div style={{ margin: '0 10px'}}>
-              <Input className='search' placeholder="Search in MyECommerce" disabled={true} />
-            </div>
-          </div>
-        </Header>
-        <Content className='main-content'>
-          <h1>{process.env.REACT_APP_API_HOST}</h1>
-        </Content>
-        <Footer className='main-footer'>
-          <h1>Footer</h1>
-        </Footer>
-      </Layout>
+          </Header>
+          <Content className='main-content'>
+            <h1>{process.env.REACT_APP_API_HOST}</h1>
+            <Switch>
+              <ProtectedRoute isAllowed={true} exact path="/profile-page" render={(props) => <ProfilePage {...props} />} />
+            </Switch>
+          </Content>
+          <Footer className='main-footer'>
+            <h1>Footer</h1>
+          </Footer>
+        </Layout>
+      </Router>
     </div>
   );
 }
